86289e196b318102c9b0d61aed7b284d2577fac0
Vitaly Zelenov <longer1386@gmail.com>
2023-03-11 00:41:58 +0500

    Task 1_3

---
+++ Lesson_1/1_3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter number: ");
+int x = int.Parse(Console.ReadLine()!);
+if (x > 0) 
+{
+    for (int i = x - x * 2; i <= x; i++ )
+    {
+        Console.Write(" " + i);
+    }
+}
+else if (x < 0)
+{
+    for (int i = x*-1; i >= x; i-- )
+    {
+        Console.Write(" " + i);
+    }
+}
+else Console.Write("Number doesn't fit");
