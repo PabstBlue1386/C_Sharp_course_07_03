91d7a3f5ac54b9fcdc1167c49631d5c98d243537
Vitaly Zelenov <longer1386@gmail.com>
2023-03-11 00:04:10 +0500

    Task 1_2

---
+++ Lesson_1/1_2/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Enter nuber 1-7: ");
+int x = int.Parse(Console.ReadLine()!);
+
+if (x == 1)
+{
+    Console.Write("Monday");
+}
+else if (x == 2) 
+{
+    Console.Write("Tuesday");
+}
+else if (x == 3)
+{
+    Console.Write("Wednesday");
+}
+else if (x == 4) 
+{
+    Console.Write("Thursday");
+}
+else if (x == 5) 
+{
+    Console.Write("Friday");
+}
+else if (x == 6)
+{
+    Console.Write("Saturday");
+}
+else if (x == 7)
+{
+    Console.Write("Sunday");
+}
+else 
+{
+    Console.Write("Number doesn't fit");
+}
