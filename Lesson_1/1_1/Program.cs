5068dce14c60f9535369e8929b44f58430d00575
Vitaly Zelenov <longer1386@gmail.com>
2023-03-10 23:47:07 +0500

    Task 1_1

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter first number: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.Write("Enter second numer: ");
+int y = int.Parse(Console.ReadLine()!);
+if(y * y == x)
+{
+    Console.Write(x + " Is square of this number " + y);  
+}
+else 
+{
+    Console.Write(x + " Is not the square of this number " + y);
+}
