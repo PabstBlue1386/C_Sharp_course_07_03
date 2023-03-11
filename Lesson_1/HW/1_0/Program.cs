18dc1dc59e1049dc135d559b49d44bd76459ff8e
Pabst <longer1386@gmail.com>
2023-03-11 23:43:18 +0500

    Added homework task 1_0 and 1_1

---
+++ Lesson_1/HW/1_0/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Задача 1: Напишите программу, которая на вход
+//  принимает два числа и выдаёт, какое число
+//  большее, а какое меньшее.
+
+Console.Write("Enter first number: ");
+int firstNumber = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter second number: ");
+int secondNumber = int.Parse(Console.ReadLine()!);
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine("The first number is greater than the second");
+}
+else if (firstNumber < secondNumber)
+{
+    Console.WriteLine("The second number is greater than the first");
+}
+else
+{
+    Console.WriteLine("The numbers are equal");
+}
---
+++ Lesson_1/HW/1_1/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 2: Напишите программу, которая принимает
+//на вход три числа и выдаёт максимальное из этих чисел
+
+Console.Write("Enter first number: ");
+int firstNumber = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter second number: ");
+int secondNumber = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter third number: ");
+int thirdNumber = int.Parse(Console.ReadLine()!);
+
+if (firstNumber > secondNumber)
+{
+    if (firstNumber > thirdNumber)
+    {
+        Console.Write("First number is maximal");
+    }
+    else
+    {
+        Console.Write("Third number is maximal");
+    }
+}
+else
+{
+    if (secondNumber > thirdNumber)
+    {
+        Console.Write("Second number is maximal");
+    }
+    else
+    {
+        Console.Write("Third number is maximal");
+    }
+}
