3cb69be220c97bb8d31d64aee25f3405d8395f9f
Pabst <longer1386@gmail.com>
2023-03-11 23:53:49 +0500

    Added HW Task 1_3

---
+++ Lesson_1/HW/1_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 3: Напишите программу, которая на вход
+//принимает число и выдаёт, является ли число
+//чётным (делится ли оно на два без остатка).
+
+
+Console.Write("Enter number: ");
+double number = double.Parse(Console.ReadLine()!);
+if (number % 2 == 0) Console.WriteLine("Number is even");
+else Console.WriteLine("Number is not even");
+
