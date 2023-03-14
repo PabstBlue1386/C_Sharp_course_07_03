e32a22ad81828b84777f074128fa46a76ecae8cb
Vitaly Zelenov <longer1386@gmail.com>
2023-03-14 16:28:02 +0500

    Added task Lesson_2/2_3

---
+++ Lesson_2/2_3/Program.cs
@@ -0,0 +1,9 @@
+﻿String Multiple(int num)
+{
+    if ((num % 7 == 0) && (num % 23 == 0 )) return $"Number {num} multiple 7 and 23";
+    else return $"Number {num} not multiple 7 and 23";
+}
+
+int number = int.Parse(Console.ReadLine()!);
+String result = Multiple(number);
+Console.WriteLine(result);
