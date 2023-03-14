de953aef132a224b8bbd50f40b7fb56fc1219eca
Vitaly Zelenov <longer1386@gmail.com>
2023-03-14 16:53:06 +0500

    Added Lesson_2/HW/2_0

---
+++ Lesson_2/HW/1_0/Program.cs
@@ -0,0 +1,16 @@
+﻿void SecondNumber(int number)
+{
+    if (number > 100 && number < 999)
+    {
+        number = number / 10 % 10;
+        Console.WriteLine(number);
+    } 
+    else 
+    {
+        Console.WriteLine("Number not three digits");
+    }
+}
+
+Console.Write("Enter number: ");
+int num = int.Parse(Console.ReadLine()!);
+SecondNumber(num);
