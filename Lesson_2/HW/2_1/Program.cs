7ded6306d10006bc17acbfac0209ed28ae12a850
Vitaly Zelenov <longer1386@gmail.com>
2023-03-14 17:39:26 +0500

    Added Lesson_2/HW/2_1

---
+++ Lesson_2/HW/2_1/Program.cs
@@ -0,0 +1,11 @@
+﻿void DellSecondNumber(int num)
+{
+    int firstNum = num / 100;
+    int secondNum = num % 10;
+    Console.Write(firstNum);
+    Console.WriteLine(secondNum);
+}
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+DellSecondNumber(number);
