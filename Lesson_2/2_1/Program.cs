edaeb74674152ebc304fb7bf346d2f346c17e4b0
Vitaly Zelenov <longer1386@gmail.com>
2023-03-14 15:23:03 +0500

    Filled out task Lesson_2/2_1

--- Lesson_2/2_1/Program.cs
+++ Lesson_2/2_1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void MaxNum(int num)
+{
+    int x = num % 10;
+    int y = num / 10;
+    if (x>y) Console.WriteLine(x);
+    else Console.WriteLine(y);
+}
+
+int number = new Random().Next(10, 100);
+Console.WriteLine(number);
+MaxNum(number);
