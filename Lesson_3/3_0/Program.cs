f4a659ac9877a3809f666e3f0a2c1ccf223c8e55
Vitaly Zelenov <longer1386@gmail.com>
2023-03-14 12:40:27 +0500

    Added Lesson_3/3_0

---
+++ Lesson_3/3_0/Program.cs
@@ -0,0 +1,12 @@
+﻿void Quarters(int x, int y)
+{
+    if (x == 0 || y == 0) Console.WriteLine("X = 0 or Y = 0");
+    else if (x> 0 && y > 0) Console.WriteLine("I");
+    else if (x< 0 && y > 0) Console.WriteLine("II");
+    else if (x< 0 && y < 0) Console.WriteLine("III");
+    else if (x > 0 && y < 0) Console.WriteLine("IV");
+}
+
+int num_1 = int.Parse(Console.ReadLine()!);
+int num_2 = int.Parse(Console.ReadLine()!);
+Quarters(num_1, num_2);
