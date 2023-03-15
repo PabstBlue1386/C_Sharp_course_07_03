3d37e47996b216ff6834094fe348a303bb84299f
Vitaly Zelenov <longer1386@gmail.com>
2023-03-16 00:26:01 +0500

    added lesson_3/hw/3_2

---
+++ Lesson_3/HW/3_2/Program.cs
@@ -0,0 +1,14 @@
+﻿void Cube(int n)
+{
+    int index = 1;
+    while (index <= n)
+    {
+        Console.Write($" {Math.Pow(index, 3)}, ");
+        index++;
+    }
+}
+
+Console.Write("Enter number N: ");
+int number = int.Parse(Console.ReadLine()!);
+
+Cube(number);
