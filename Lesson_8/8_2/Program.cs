af75182ee924fead619a0f5b1a2aa1029bde175d
Vitaly Zelenov <longer1386@gmail.com>
2023-04-05 13:28:25 +0500

    added lesson_8/8_2

---
+++ Lesson_8/8_2/Program.cs
@@ -0,0 +1,64 @@
+﻿int[,] GenerateRandomArray()
+{
+    Console.Write("Enter number rows: ");
+    int rows = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter number columns: ");
+    int columns = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter down border mass: ");
+    int downBorder = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter up border mass: ");
+    int upBorder = int.Parse(Console.ReadLine()!);
+    int[,] arr = new int[rows, columns];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(downBorder, upBorder + 1);
+        }
+    }
+
+    return arr;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" [{array[i, j]}]");
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
+
+int[,] NewArray(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = i; j < columns; j++)
+        {
+            (array[i, j], array[j, i]) = (array[j, i], array[i, j]);
+        }
+
+    }
+    return array;
+}
+
+int[,] matrix = GenerateRandomArray();
+PrintArray(matrix);
+
+if (matrix.GetLength(0) == matrix.GetLength(1))
+{
+    int[,] resultArr = NewArray(matrix);
+    PrintArray(resultArr);
+
+}
+else
+{
+    Console.WriteLine("The number of rows and columns is different, swapping will not work!");
+}
