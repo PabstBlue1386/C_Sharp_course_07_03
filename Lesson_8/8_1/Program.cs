76ab3dddf7b45b7ac1022b05bffd9a686b763dee
Vitaly Zelenov <longer1386@gmail.com>
2023-04-05 12:43:50 +0500

    added lesson_8/8_1

---
+++ Lesson_8/8_1/Program.cs
@@ -0,0 +1,57 @@
+﻿int[,] GenerateRandomArray()
+{
+    Console.Write("Enter number rows: ");
+    int rows = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter number columns: ");
+    int columns = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter down border mass: ");
+    int downBorder = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter up border mass: ");
+    int upBorder = int.Parse(Console.ReadLine()!);
+    int[,] arr = new int[rows, columns];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(downBorder, upBorder + 1);
+        }
+    }
+
+    return arr;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" [{array[i, j]}]");
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
+
+int[,] NewArray(int[,] array)
+{
+    int lastRow = array.GetLength(0);
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+
+        (array[0, j], array[lastRow - 1, j]) = (array[lastRow - 1, j], array[0, j]);
+    }
+    return array;
+}
+
+int[,] matrix = GenerateRandomArray();
+PrintArray(matrix);
+
+int[,] resultArr = NewArray(matrix);
+PrintArray(resultArr);
+
+
+
+
+
