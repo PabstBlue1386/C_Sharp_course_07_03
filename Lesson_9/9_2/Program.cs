c80a23a5fc6c207243dbe6617330f6ee384f6f8a
Vitaly Zelenov <longer1386@gmail.com>
2023-04-07 20:23:37 +0500

    Added Lesson_9/9_2

---
+++ Lesson_9/9_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Enter N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+NaturalDigit(m,n);
+
+void NaturalDigit(int min, int max)
+{
+    if (min-1 == max) return;
+    NaturalDigit(min, max-1 );
+    Console.Write($" {max} ");
+}
