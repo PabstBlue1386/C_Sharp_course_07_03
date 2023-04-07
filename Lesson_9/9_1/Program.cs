07bae1fc29bc9628e2144bddb2bf7d936a4d7a88
Vitaly Zelenov <longer1386@gmail.com>
2023-04-07 19:53:43 +0500

    Added Lesson_9/9_1

---
+++ Lesson_9/9_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter N: ");
+int n = int.Parse(Console.ReadLine()!);
+NaturalDigitN(n);
+void NaturalDigitN(int n)
+{
+    if (n==0) return;
+    
+    NaturalDigitN(n-1);
+    Console.Write($" {n} ");
+    
+}
