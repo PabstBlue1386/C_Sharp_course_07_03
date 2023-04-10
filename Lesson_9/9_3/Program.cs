583a537e2705971f5cda7d94c0be00aea73bf16d
Vitaly Zelenov <longer1386@gmail.com>
2023-04-10 22:32:10 +0500

    Added Lesson_9/9_3

---
+++ Lesson_9/9_3/Program.cs
@@ -0,0 +1,8 @@
+﻿int Sum(int a) 
+{
+    if (a<10) return a;
+    return Sum(a/10) + a %10;
+}
+Console.Write("Enter number: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Sum -> {Sum(num)}");
