b790dfc366b0e1381f516ef1890aa2960d681ba5
Vitaly Zelenov <longer1386@gmail.com>
2023-04-11 00:02:54 +0500

    Added Lesson_9/hw/9_1

---
+++ Lesson_9/HW/9_1/Program.cs
@@ -0,0 +1,17 @@
+﻿void NatNum(int a, int b)
+{
+    if (a <= b)
+    {
+        if (a % 2 == 0)
+        {
+            Console.Write($" {a}");
+        }
+        NatNum(a + 1, b);
+    }
+}
+
+Console.Write("Enter number A: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter number B: ");
+int num2 = int.Parse(Console.ReadLine()!);
+NatNum(num,num2);
